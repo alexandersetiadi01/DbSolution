209b552791f20ffac784222ea4e8f32b9430cae4
alexandersetiadi01 <alexandersetiadi01@gmail.com>
2022-10-17 14:27:01 +0700

    page logistik & RAP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/navbar/Footer';
 import Header from './components/navbar/Header';
 import Sidebar from './components/navbar/Sidebar';
+import Login from "./components/pages/user/login";
+import BarangMasuk from "./components/pages/logistik/barangMasuk";
+import BarangKeluar from "./components/pages/logistik/barangKeluar";
+import Inventory from "./components/pages/logistik/Inventory";
+import RAP from './components/pages/RAP/RAP';
+import Progress from './components/pages/RAP/Progress';
 
 function App() {
   return (
     <div className="wrapper">
       <Header/>
       <Sidebar/>
+      <Routes>
+
+        <Route path="/Login" element={<Login />}></Route>
+
+        {/* User */}
+    
+        {/* logistik */}
+        <Route path="/Logistik/barangMasuk" element={<BarangMasuk />}></Route>
+        <Route path="/Logistik/barangKeluar" element={<BarangKeluar />}></Route>
+        <Route path="/Logistik/Inventory" element={<Inventory />}></Route>
+
+        {/* RAP */}
+        <Route path="/RAP/detailRAP" element={<RAP />}></Route>
+        <Route path="/RAP/Progress" element={<Progress />}></Route>
+
+        <Route path="/" element={<Login />}></Route>
+      </Routes>
       <Footer/>
     </div>
   );
---
+++ src/components/pages/logistik/barangMasuk.js
@@ -0,0 +1,126 @@
+import React from "react";
+
+function BarangMasuk(props) {
+  return (
+    <div className="content-wrapper">
+      {/* Content Header (Page header) */}
+      <section className="content-header">
+        <div className="container-fluid">
+          <div className="row mb-2">
+            <div className="col-sm-6">
+              <h1>Barang Masuk</h1>
+            </div>
+            <div className="col-sm-6">
+              <ol className="breadcrumb float-sm-right">
+                <li className="breadcrumb-item">
+                  <a href="#">Logistik</a>
+                </li>
+                <li className="breadcrumb-item active">Barang Masuk</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+        {/* /.container-fluid */}
+      </section>
+      <section className="content">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <div className="card">
+                <div className="card-header">
+                  <h3 className="card-title">
+                    List Barang Masuk
+                  </h3>
+                </div>
+                {/* /.card-header */}
+                <div className="card-body">
+                  <table
+                    id="example1"
+                    className="table table-bordered table-striped"
+                  >
+                    <thead>
+                      <tr>
+                        <th>Rendering engine</th>
+                        <th>Browser</th>
+                        <th>Platform(s)</th>
+                        <th>Engine version</th>
+                        <th>CSS grade</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Misc</td>
+                        <td>NetFront 3.4</td>
+                        <td>Embedded devices</td>
+                        <td>-</td>
+                        <td>A</td>
+                      </tr>
+                      <tr>
+                        <td>Misc</td>
+                        <td>Dillo 0.8</td>
+                        <td>Embedded devices</td>
+                        <td>-</td>
+                        <td>X</td>
+                      </tr>
+                      <tr>
+                        <td>Misc</td>
+                        <td>Links</td>
+                        <td>Text only</td>
+                        <td>-</td>
+                        <td>X</td>
+                      </tr>
+                      <tr>
+                        <td>Misc</td>
+                        <td>Lynx</td>
+                        <td>Text only</td>
+                        <td>-</td>
+                        <td>X</td>
+                      </tr>
+                      <tr>
+                        <td>Misc</td>
+                        <td>IE Mobile</td>
+                        <td>Windows Mobile 6</td>
+                        <td>-</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Misc</td>
+                        <td>PSP browser</td>
+                        <td>PSP</td>
+                        <td>-</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Other browsers</td>
+                        <td>All others</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>U</td>
+                      </tr>
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <th>Rendering engine</th>
+                        <th>Browser</th>
+                        <th>Platform(s)</th>
+                        <th>Engine version</th>
+                        <th>CSS grade</th>
+                      </tr>
+                    </tfoot>
+                  </table>
+                </div>
+                {/* /.card-body */}
+              </div>
+              {/* /.card */}
+            </div>
+            {/* /.col */}
+          </div>
+          {/* /.row */}
+        </div>
+        {/* /.container-fluid */}
+      </section>
+    </div>
+  );
+}
+
+export default BarangMasuk;
